5ccad4f2f864758caaa546fffe10d1622de1557e
LoekD <loekd@hotmail.com>
2022-09-15 21:11:00 +0200

    feat(login): allow multiple metadata endpoints

--- XpiritInsurance.Api/Program.cs
+++ XpiritInsurance.Api/Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.Identity.Web;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols;
+using System.IdentityModel.Tokens.Jwt;
+using System.Collections.Concurrent;
 
 namespace XpiritInsurance.Api;
 
@@ -12,14 +19,30 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        var configurationManagers = new ConcurrentDictionary<string, ConfigurationManager<OpenIdConnectConfiguration>>();
+
+        //authentication config
         builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                     .AddMicrosoftIdentityWebApi(options =>
                     {
                         builder.Configuration.Bind("AzureAdB2C", options);
                         options.TokenValidationParameters.NameClaimType = "name";
+                        options.TokenValidationParameters.ValidateIssuerSigningKey = false;
+                        options.TokenValidationParameters.IssuerSigningKeyResolver = (token, securityToken, kid, validationParameters) =>
+                        {
+                            //use cached info from metadata endpoint to fetch key info for signature validation
+                            var tfpClaim = ((JwtSecurityToken)securityToken).Claims.Single(c => c.Type == "tfp");
+                            string stsDiscoveryEndpoint = $"https://xpiritinsurance.b2clogin.com/xpiritinsurance.onmicrosoft.com/{tfpClaim.Value}/v2.0/.well-known/openid-configuration";
+
+                            ConfigurationManager<OpenIdConnectConfiguration> configManager = configurationManagers.GetOrAdd(kid, key => new(stsDiscoveryEndpoint, new OpenIdConnectConfigurationRetriever()));
+                            var config = configManager.GetConfigurationAsync(CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                            return config.JsonWebKeySet.GetSigningKeys();
+                        };
                     },
                     options => { builder.Configuration.Bind("AzureAdB2C", options); });
 
+        //allow both front-ends
         builder.Services.AddCors(options =>
         {
             options.AddPolicy(name: CorsPolicyName,
@@ -32,11 +55,9 @@ public class Program
         });
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-
-
 
         //Add mock services
         builder.Services.AddSingleton<Services.QuoteAmountService>();
@@ -44,8 +65,6 @@ public class Program
         builder.Services.AddSingleton<Services.DamageClaimService>();
 
         var app = builder.Build();
-
-
 
         app.UseHttpsRedirection();
         app.UseCors(CorsPolicyName);
@@ -56,6 +75,7 @@ public class Program
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
+            IdentityModelEventSource.ShowPII = true;
             app.UseSwagger();
             app.UseSwaggerUI();
         }

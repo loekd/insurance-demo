421ecf020481433ca453d0322bf464e38b3d1593
LoekD <loekd@hotmail.com>
2023-11-02 07:39:25 +0100

    feat(qa): bump all packages

--- XpiritInsurance.Api/Program.cs
+++ XpiritInsurance.Api/Program.cs
@@ -1,14 +1,11 @@
 using Microsoft.Identity.Web;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Protocols;
-using System.IdentityModel.Tokens.Jwt;
 using System.Collections.Concurrent;
 using System.Diagnostics;
-using System;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace XpiritInsurance.Api;
 
@@ -20,6 +17,8 @@ public class Program
     {
         if (args.Any(a=> a.Contains("dapr")))
         {
+            //attach dapr sidecar to this process on the fly
+            //it watches this process and will stop when it exits
             var thisProces = Process.GetCurrentProcess();
 
             var psi = new ProcessStartInfo("dotnet")
@@ -45,8 +44,9 @@ public class Program
                         options.TokenValidationParameters.ValidateIssuerSigningKey = true;
                         options.TokenValidationParameters.IssuerSigningKeyResolver = (token, securityToken, kid, validationParameters) =>
                         {
+                            //used to support both user flows and custom policies
                             //use cached info from metadata endpoint to fetch key info for signature validation
-                            var tfpClaim = ((JwtSecurityToken)securityToken).Claims.Single(c => c.Type == "tfp");
+                            var tfpClaim = ((JsonWebToken)securityToken).Claims.Single(c => c.Type == "tfp");
                             string stsDiscoveryEndpoint = $"https://{builder.Configuration["AzureAdB2C:TenantName"]}.b2clogin.com/{builder.Configuration["AzureAdB2C:Domain"]}/{tfpClaim.Value}/v2.0/.well-known/openid-configuration";
                             ConfigurationManager<OpenIdConnectConfiguration> configManager = configurationManagers.GetOrAdd(kid, key => new(stsDiscoveryEndpoint, new OpenIdConnectConfigurationRetriever()));
                             var config = configManager.GetConfigurationAsync(CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
